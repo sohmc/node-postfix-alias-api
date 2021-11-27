75ca7cf04b158a95848a3ee349ccbdbc0beed2ed
Michael Soh <2767724+sohmc@users.noreply.github.com>
2021-11-27 22:36:37 +0000

    added sqlite library

--- database-schemas/sqlite.sql
+++ database-schemas/sqlite.sql
@@ -4,13 +4,13 @@ CREATE TABLE domains (
   description TEXT,
   created_at DATETIME NOT NULL,
   updated_at DATETIME NOT NULL,
-  active INTEGER NOT NULL,
+  active INTEGER NOT NULL
 );
 
 CREATE TABLE destinations (
   destination_id INTEGER PRIMARY KEY,
   destination TEXT NOT NULL,
-  active INTEGER NOT NULL,
+  active INTEGER NOT NULL
 );
 
 CREATE TABLE aliases (
@@ -18,12 +18,12 @@ CREATE TABLE aliases (
   uuid TEXT NOT NULL,
   alias_address TEXT NOT NULL,
   domain_id INTEGER NOT NULL,
-  FOREIGN KEY(domain_id) REFERENCES domains(domainId) ON DELETE RESTRICT,
   destination_id INTEGER NOT NULL,
-  FOREIGN KEY(destination_id) REFERENCES destinations(destination_id) ON DELETE RESTRICT,
   created_at DATETIME NOT NULL,
   updated_at DATETIME NOT NULL,
   active INTEGER NOT NULL DEFAULT 1,
   ignore INTEGER NOT NULL DEFAULT 0,
   count INTEGER NOT NULL DEFAULT 0,
+  FOREIGN KEY(domain_id) REFERENCES domains(domainId) ON DELETE RESTRICT,
+  FOREIGN KEY(destination_id) REFERENCES destinations(destination_id) ON DELETE RESTRICT
 );
