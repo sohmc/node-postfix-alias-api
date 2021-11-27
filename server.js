f662f8067df0ceee326dfb61765fab4c4b962bbe
Michael Soh <2767724+sohmc@users.noreply.github.com>
2021-11-27 23:39:41 +0000

    Attach logger and db handler with the request

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
+const sqlite3 = require("better-sqlite3");
+
+// Winston Logger Declarations
+const winston = require('winston');
+var log_level = process.env.LOG_LEVEL || 'info';
+
+const logger = winston.createLogger({
+  level: log_level,
+  transports: [
+    new winston.transports.Console({format: winston.format.combine(winston.format.colorize(), winston.format.simple())}),
+    new winston.transports.File({filename: 'logs/combined.log'})
+  ]
+});
+
+var db = undefined;
+try {
+  db = new sqlite3(process.env.DBFILE, {fileMustExist: true});
+} catch (connectionError) {
+  logger.error(connectionError);
+  process.exit(1);
+}
+
 var restify = require('restify');
 const routes = require('./routes');
 
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
-
 var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
 
 server.get('/domain', routes.domain.getDomains);
+server.pre(function (req, res, next) {
+  req.db = db;
+  req.logger = logger;
+  next();
+});
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
